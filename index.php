63b930d12bc7a14bfc59f2d65d03abbf3930fea8
patapata23 <sinner315@gmail.com>
2016-10-01 18:14:50 +0900

    修正

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 $json_string = file_get_contents('php://input');
 $json_object = json_decode($json_string);
 
+echo $json_string;
+
 foreach ($json_object->events as $event) {
      if('message' == $event->type){
            api_post_request($event->replyToken, $event->message->text);
@@ -26,12 +28,12 @@ function api_post_request($token, $message) {
                                 'text' => $message
                               )
                              )
-               );
+       );
 
-         $curl = curl_init($url);
-         curl_setopt($curl, CURLOPT_POST, true);
-         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post));
-         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-         curl_exec($curl);
+       $curl = curl_init($url);
+       curl_setopt($curl, CURLOPT_POST, true);
+       curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+       curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post));
+       curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+       curl_exec($curl);
 }
