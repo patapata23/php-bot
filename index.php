133682c30c93ba6cb4833cf98e65894a3c853619
patapata23 <sinner315@gmail.com>
2016-10-04 18:43:19 +0900

    beaconイベント修正

--- index.php
+++ index.php
@@ -7,20 +7,32 @@ echo $json_string;
 
 foreach ($json_object->events as $event) {
      if('message' == $event->type){
-           api_post_request($event->replyToken, $event->message->text);
+           api_post_request($event->replyToken, $event->message->text, $event->type);
      }else if('beacon' == $event->type){
-           api_post_request($event->replyToken, 'BEACONイベント!!');
+           api_post_request($event->replyToken, 'BEACONイベント!!', $event->type);
      }
 }
 
-function api_post_request($token, $message) {
+function api_post_request($token, $message, $type) {
        $url = 'https://api.line.me/v2/bot/message/reply';
        $channel_access_token = 'ltj/YgPzQIGcUjDDGWOC2bTP8WjZCBD0S/COyVi6knuMwJJb8xNdTwrc61XILB8VOr35vrDUyU4sQzK8ZXOj7fhX2w9gPZ/FaTDPxrY7VUHuqrDc9+cmgFWRVrojc0KR+lydSt97n/u0oRVSq5kvcQdB04t89/1O/w1cDnyilFU=';
        $headers = array(
                   'Content-Type: application/json',
                   "Authorization: Bearer {$channel_access_token}"
-                  );
-       $post = array(
+          );
+       if ($type == 'beacon') {
+           $post = array(
+                 'replyToken' => $token,
+                 'messages' => array(
+                              array(
+                                'type' => 'video',
+                                 'originalContentUrl' => 'https://example.com/original.mp4',
+                                     "previewImageUrl" => "https://example.com/preview.jpg"
+                              )
+                    )
+                );
+       } else {
+           $post = array(
                  'replyToken' => $token,
                  'messages' => array(
                               array(
@@ -28,7 +40,8 @@ function api_post_request($token, $message) {
                                 'text' => $message
                          )
                  )
-       );
+           );
+       } 
 
 
        $curl = curl_init($url);
